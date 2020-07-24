59ae39a02d4794fd33ccb21c05df3c164fbaf6de
stevie2codes <stevie2codes@gmail.com>
2020-07-24 16:06:25 -0500

    buliding storybook

---
+++ storybook-static/precache-manifest.63f126c363679ce5742cb7385b0fa499.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21561770a9255eb0fe5ef11a3854be61",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.4a139a67f12ecf4603b8.bundle.js"
+  },
+  {
+    "url": "runtime~main.4a139a67f12ecf4603b8.bundle.js"
+  },
+  {
+    "revision": "7fca4ae9e0117131c2a04fb8a2f1eca4",
+    "url": "static/media/signin.7fca4ae9.svg"
+  },
+  {
+    "url": "vendors~main.4a139a67f12ecf4603b8.bundle.js"
+  },
+  {
+    "url": "vendors~main.4a139a67f12ecf4603b8.bundle.js.LICENSE.txt"
+  }
+]);
